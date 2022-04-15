48106ce73e17236fe24e1eda1830ea4a2ac260b4
Jon Giarla <jonathangiarla@gmail.com>
2022-04-15 07:20:00 -0400

    V 0.2 Got basic book layout set in grid form of library.  Set variables for script.

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+const bookTitle = document.getElementById(bookTitle);
+const bookAuthor = document.getElementById(bookAuthor);
+const bookPages = document.getElementById(bookPages);
+const bookRead = document.getElementById(bookRead);
+const submitButton = document.getElementById(submit);
+const bookDisplay = document.getElementById(bookDisplay);
+
 let myLibary = [];
 
-function Book() {
-    // the constructor
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary() {
