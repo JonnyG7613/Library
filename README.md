4bd3c490c2f1f7af8f211785b1417e13662f7ef7
Jon Giarla <jonathangiarla@gmail.com>
2022-04-17 10:03:05 -0400

    V 0.3 Fine tuned placement of text and checkbox on sidebar.  Adjusted button size and text.  Successfully programmed library to add a book.  Programmed two icons on each book, one to change the book's read status and another to delete the book.  Next to add will be local/cloud storage to make the library fully usable.

--- README.md
+++ README.md
@@ -4,5 +4,9 @@ V 0.1 Layout begun.  Basic CSS groundwork laid.  Inputs created.
 
 V 0.1.1 Modified layout.  Pretty sick so I'm going to work on it later when I feel better.
 
+V 0.2 Got basic book layout set in grid form of library.  Set variables for script.
+
+V 0.3 Fine tuned placement of text and checkbox on sidebar.  Adjusted button size and text.  Successfully programmed library to add a book.  Programmed two icons on each book, one to change the book's read status and another to delete the book.  Next to add will be local/cloud storage to make the library fully usable.
+
 # Library
 A library created for The Odin Project that allows books to be added to a virtual library and their info stored.
--- script.js
+++ script.js
@@ -1,23 +1,81 @@
-const bookTitle = document.getElementById(bookTitle);
-const bookAuthor = document.getElementById(bookAuthor);
-const bookPages = document.getElementById(bookPages);
-const bookRead = document.getElementById(bookRead);
-const submitButton = document.getElementById(submit);
-const bookDisplay = document.getElementById(bookDisplay);
+let bookTitle = document.getElementById('bookTitle')
+let bookAuthor = document.getElementById('bookAuthor')
+let bookPages = document.getElementById('bookPages')
+let bookRead = document.getElementById('bookRead')
+const submitButton = document.getElementById('submit')
+let bookDisplay = document.getElementById('bookDisplay')
+let del = document.querySelector('.delete')
+let read = document.querySelector('.read')
 
-let myLibary = [];
+let myLibrary = []
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-
-function addBookToLibrary() {
-    // do stuff here
+function addBookToLibrary(title, author, pages, read) {
+    if (read.checked) {
+        myLibrary.push([title, author, pages, "Read"])
+    } else {
+        myLibrary.push([title, author, pages, "Not read"])
+    }
 }
 
 function displayLibrary() {
-    // do stuff here
+    console.log(`myLibrary length is ${myLibrary.length}`)
+    let books = document.querySelectorAll('.book')
+    books.forEach(book => {
+        book.remove()
+    })
+    for (let i = 0; i < myLibrary.length; i++) {
+        let bookHTML = `<p>${myLibrary[i][0]}</p>` +
+            `<p>${myLibrary[i][1]}</p>` +
+            `<p>${myLibrary[i][2]} pgs</p>` +
+            `<p>${myLibrary[i][3]}</p>` +
+            `<img src="./delete.png" alt="delete" class="delete">` +
+            `<img src="./book-open-blank-variant.png" alt="read" class="read"></img>`
+        let g = document.createElement('div')
+        g.setAttribute('class', 'book')
+        g.innerHTML = bookHTML
+        bookDisplay.appendChild(g)
+    }
 }
+
+submitButton.onclick = function () {
+    addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value, bookRead)
+    displayLibrary()
+}
+
+// When clicking the book icon in the book class in the Library, it changes
+//   the status of the book from Read to Not read and vice versa
+
+document.querySelector('body').addEventListener('click', function (e) {
+    if (e.target.className == 'read') {
+        console.log('read button test')
+        let temp = e.target.parentNode.previousSibling
+        let count = 0
+        console.log(`previous sibling is ${temp}`)
+        while (temp.className == 'book') {
+            count++
+            temp = temp.previousSibling
+        }
+        if (myLibrary[count][3] == 'Read') {
+            myLibrary[count][3] = 'Not read'
+        } else {
+            myLibrary[count][3] = 'Read'
+        }
+        displayLibrary()
+    }
+})
+
+
+document.querySelector('body').addEventListener('click', function (e) {
+    if (e.target.className == 'delete') {
+        console.log('delete button test')
+        let temp = e.target.parentNode.previousSibling
+        let count = 0
+        console.log(`previous sibling is ${temp}`)
+        while (temp.className == 'book') {
+            count++
+            temp = temp.previousSibling
+        }
+        myLibrary.splice(count, 1)
+        displayLibrary()
+    }
+})
